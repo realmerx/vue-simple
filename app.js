f6327124eb23c3cbd3785de6c9635c45afdebb06
phade <realmerx@gmail.com>
2018-11-21 01:38:38 +0200

    Code formatting

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ new Vue({
     },
     el: '#app',
     created: function () {
-        
         axios.get('http://www.mocky.io/v2/5bf4909c310000f928619d6e').then((response) => {
-        this.medias = response.data.movies;
+            this.medias = response.data.movies;
         });    
     }
 });
